eb3d82ad87be251ce994f6a5f0319f442cb9b200
Szymon <szymon.zsp5@gmail.com>
2020-12-10 20:24:00 +0100

    final test

--- screens/testScreen.js
+++ screens/testScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity, Modal} from 'react-native';
 import Navbar from "../componets/navbar";
 
 const task = [
@@ -52,11 +52,13 @@ const task = [
 
 class TestScreen extends React.Component {
     state = {
-        test: {},
-        currQuestion: 0,
-        currScore: 0,
+        test: {answers: []},
+        testIndex: -1,
+        points: 0,
+        duration: 30,
+        loaded: false,
+        completed: false,
         testNumber: this.props.route.params.testNumber,
-        duration: 30
     }
 
     componentDidMount() {
@@ -64,42 +66,81 @@ class TestScreen extends React.Component {
     }
 
     loadTest = () => {
-        this.setState({test:task[0]})
+
+        let { testIndex } = this.state;
+
+        testIndex = testIndex + 1;
+
+        if (task.length === testIndex){
+            this.setState({
+                ...this.state,
+                completed: true,
+                loaded: false
+            })
+        }else {
+            this.setState({
+                ...this.state,
+                test: task[testIndex],
+                duration: task[testIndex].duration,
+                loaded: false
+            })
+        }
+    }
+
+    handleNextTask = () => {
+        let { testIndex } = this.state;
+        testIndex = testIndex + 1;
+        this.setState({
+            ...this.state,
+            testIndex
+        }, () => {
+            this.loadTest();
+        })
+    }
+
+    markAnswer = (id) => {
+        const { answers } = this.state.test;
+        let { points } = this.state;
+
+        if (answers[id].isCorrect === true ){
+            points +=1;
+        }
+
+        this.setState({
+            ...this.state,
+            points,
+            loaded: false,
+        }, ()=> this.handleNextTask())
     }
 
     //TODO do dokonczenia!
     render() {
         const {title, testNumber} = this.props.route.params;
-        const {test,currQuestion,currScore,duration} = this.state;
+        // const {test,currQuestion,currScore,duration} = this.state;
         return (
             <View style={{flex: 1}}>
                 <Navbar navigation={this.props.navigation} title={title}/>
                 <View style={styles.first}>
-                    <Text style={styles.firstText}>Question {currQuestion} of {test.length}</Text>
-                    <Text style={styles.firstText}>Time: {duration}</Text>
+                    <Text style={styles.firstText}>Question {this.state.testIndex+2} of {task.length}</Text>
+                    <Text style={styles.firstText}>Time: {this.state.duration}</Text>
                 </View>
                 <View style={styles.second}>
-                    <Text style={styles.firstText}>{task[0].question}</Text>
-                    <Text numberOfLines={2} style={styles.secondText}>Lorem ipsum dolor sit amet, consectetur adipiscing
-                        elit. Phasellus diam felis, dapibus vel purus id, iaculis euismod mi.
-                        Duis nunc velit, elementum sed fermentum vitae, dignissim non urna. Integer quis odio
-                        elementum lacus viverra fringilla. Morbi in feugiat sem. Integer convallis commodo est. Donec
-                        sagittis luctus mattis.</Text>
+                    <Text style={styles.firstText}>{this.state.test.question}</Text>
                 </View>
                 <View style={styles.bottomAnswer}>
-                    <TouchableOpacity style={styles.answer}><
-                        Text style={styles.answerText}>{test.answer[0]}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.answer}>
-                        <Text style={styles.answerText}>Answer B</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.answer}><
-                        Text style={styles.answerText}>Answer C</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.answer}>
-                        <Text style={styles.answerText}>Answer D</Text>
-                    </TouchableOpacity>
+                    {console.log(this.state)}
+                    {this.state.test.answers.map((val,index) => {
+                        return (<TouchableOpacity onPress={() => this.markAnswer(index)} style={styles.answer}>
+                            <Text style={styles.answerText}>{val.content}</Text>
+                        </TouchableOpacity>)
+                    })
+                    }
                 </View>
+                {this.state.completed &&
+                <View>
+                    <Text>Gratulacje!{this.state.points}</Text>
+                </View>
+                }
             </View>
         );
     }
